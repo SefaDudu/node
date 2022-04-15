e2836dec69f688e30642dda44a8add9c10d20d23
sefa <dudu.sefaa@gmail.com>
2022-04-15 16:16:57 +0300

    mvc

---
+++ final-1/todo/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose=require("mongoose");
+const app = express();
+const PORT = 3000;
+const exhbs=require("express-handlebars");
+const todoRoutes = require("./routes/todos")
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = "mongodb+srv://sefadudu:dudusefa@cluster0.a6tyc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const hbs = exhbs.create({
+
+    defaultLayout:'main',
+    extname:'hbs'
+    
+    })
+    app.engine('hbs',hbs.engine)
+    app.set('view engine', 'hbs');
+    app.set('views', './views');
+
+app.use(todoRoutes);
+async function start(){
+try {
+    await mongoose.connect(uri,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+
+    })
+
+    
+app.listen(PORT,()=>{
+    console.log("Server Running")
+})
+
+} catch (error) {
+    console.log(error)
+}
+
+}
+
+
+start();
